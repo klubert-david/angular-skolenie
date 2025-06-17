af8cd28a8d5a0dbbc4cd5b587c2605f53266a9ea
klubert-david <klubert.david@gmail.com>
2025-06-17 16:08:29 +0200

    zadanie02

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, ProductListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'moj-obchod';
+}
---
+++ src/app/header/nadpis/nadpis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nadpis',
+  imports: [],
+  templateUrl: './nadpis.component.html',
+  styleUrl: './nadpis.component.css',
+})
+export class NadpisComponent {
+  title: string = "Dave's shop";
+}
